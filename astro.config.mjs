a2b573e6470516153a9f602bb3ef8c183124cce7
JakePIXL <jakewjevans@gmail.com>
2023-01-25 02:15:59 -0500

    created landing page

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import fonts from 'astro-fonts-next';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), fonts({url: 'https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap'})]
+});
