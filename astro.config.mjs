2c0ed2b1fbd8d2d949d5b2df477ee070df0ba2c1
JakePIXL <jakewjevans@gmail.com>
2023-01-31 22:32:28 -0500

    added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import fonts from 'astro-fonts-next';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), fonts({url: 'https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap'})]
+  integrations: [tailwind(), fonts({
+    url: 'https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
