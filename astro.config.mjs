007821a8ebf38a8b6aa3300b493aceec3417f138
JakePIXL <jakewjevans@gmail.com>
2023-01-31 22:44:17 -0500

    actually removed fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import fonts from 'astro-fonts-next';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -9,9 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), fonts({
-    url: 'https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap'
-  })],
+  integrations: [tailwind()],
   output: "server",
   adapter: cloudflare()
 });
